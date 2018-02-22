8b38e0c3507736f0510c35329479b545b1edaca2
Gaut9a8m <gaut9a8m@gmail.com>
2018-02-22 21:28:17 +0530

    second commit

--- js/custom.js
+++ js/custom.js
@@ -35,3 +35,35 @@
 				autoplayHoverPause:true,
 			});
 		});
+
+			/*Navigation*/
+			//show and hide navbar
+			$(function(){
+				$(window).scroll(function(){
+					if ($(this).scrollTop()<50) {
+						//hide navbar
+						$("nav").removeClass("top-nav"); 
+						$("#back-to-top").fadeOut();
+					}
+					else{
+						//show navbar
+						$("nav").addClass("top-nav");
+						$("#back-to-top").fadeIn(); 
+					}
+
+				});
+			});
+
+				/*Smooth scrolling*/
+
+				$(function () {
+					$(".smooth-scroll").click(function(){
+						event.preventDefault();
+						//get/retrun id like #about,#work etc
+						var section = $(this).attr("href");
+
+						$('html,body').animate({
+							scrollTop: $(section).offset().top-30
+						},1250,"easeInOutExpo");
+					});
+				});
